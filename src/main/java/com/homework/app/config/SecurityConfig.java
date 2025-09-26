cfaecd6943b1aa6612ce6da9399bdb663ce191ef
Andrii Kurkov <kurkovandrey90@gmail.com>
2025-09-26 19:40:33 +0300

    fix after review

--- src/main/java/com/homework/app/config/SecurityConfig.java
+++ src/main/java/com/homework/app/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.homework.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -27,7 +27,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/login",
+                                "/logout"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
                 .formLogin(form -> form.defaultSuccessUrl("/note/list", true))
                 .logout(logout -> logout.logoutSuccessUrl("/login?logout"));
         return http.build();
